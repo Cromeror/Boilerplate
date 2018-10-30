76361b8a4a1f2c06312b0eb04479d3f1c099d065
Cristobal Romero <cristobalromerorossi@gmail.com>
2018-10-29 21:34:35 -0500

    Initial commit

---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+//Components
+//Styles
+import "../styles/styles.css";
+
+class Home extends Component {
+
+  render() {
+    return (
+    <div>
+      Hola mundo
+    </div>);
+  }
+}
+
+export default (Home);
